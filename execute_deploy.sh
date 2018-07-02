831b5a277dfc554bc523bbbbf300c35a0390ff6b
Andreas Groll <andreas.groll@gmail.com>
2018-07-02 20:27:42 +0200

    added Jenkinsfile

--- execute_deploy.sh
+++ execute_deploy.sh
@@ -2,5 +2,5 @@
 cd cfn
 chmod +x prepare_template.sh
 ./prepare_template.sh
-aws cloudformation package --template aws-resources.yml --s3-bucket nc-infrastructure-infrabucket-1qrzmjyascfar --output-template template-export.yml
+aws cloudformation package --template aws-resources.yml --s3-bucket nc-projects-infrabucket --output-template template-export.yml
 aws cloudformation deploy  --template-file=template-export.yml --stack-name="${STACK_NAME}" --parameter-overrides ParamLambdaSecret=${STACK_PARAM_PASS} --capabilities=CAPABILITY_NAMED_IAM
